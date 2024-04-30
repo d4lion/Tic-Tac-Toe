9839ab58d50d34eb8f60ccf846ef012a989469af
daliondev <d4lion.dev@gmail.com>
2024-04-30 13:19:37 -0500

    Create App(main) component

---
+++ src/App.jsx
@@ -0,0 +1,55 @@
+import { Game } from "./Game.jsx"
+import { useEffect, useState } from "react"
+
+// importacion de musica y dependencias de musica 
+import useSound from "use-sound"
+import KahootSound from "../public/KahootSound.mp3"
+import Sound from "../public/sound-off.svg"
+import MutedSong from "../public/sound.svg"
+
+
+export function App() { 
+
+  const [play, { stop }] = useSound(KahootSound, {
+    volume: 0.2,
+    interrupt: true
+  })
+
+  const [isPlaying, setIsPlaying] = useState(false)
+
+
+  const playFunction = () => {
+    const newPlayingState = isPlaying
+
+    newPlayingState ? stop() : play()
+  
+    setIsPlaying(!newPlayingState)
+  }
+
+  const PlayingButton = () => {
+
+    return (
+      <button
+          onClick={playFunction}
+          className="play-music-button"
+      >
+      
+        <img src={isPlaying ? Sound : MutedSong} height={30}/>
+          
+      </button>
+    )
+  }
+
+
+
+  return (
+    <>
+      <Game />
+      <PlayingButton/>
+  
+    </>
+  )
+}
+
+
+export default App
